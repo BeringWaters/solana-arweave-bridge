7c4c367341f23a9a035a4df34bbff57b04a37bc2
koteld <festbot@gmail.com>
2020-09-21 11:51:45 +0700

    arweave and solana transactions module refactoring (named as worker now)

--- src/worker/Arweave.worker.ts
+++ src/worker/Arweave.worker.ts
@@ -1,60 +1,69 @@
-import { ConfirmedBlock } from "@solana/web3.js";
-import * as dotenv from "dotenv";
-import { Queue, Worker } from "bullmq";
-
+import { ConfirmedBlock, ConfirmedTransactionMeta, Transaction } from '@solana/web3.js';
+import * as dotenv from 'dotenv';
+import { Queue, Worker } from 'bullmq';
+import { getTxsTags } from '../service/Arweave.tag.service';
+import { getTransactionPrice } from '../api/Arweave.api';
 import * as Redis from 'ioredis';
+import { ARWEAVE_OPTIONS, ARWEAVE_ADDRESS, WINSTON_TO_AR } from '../config';
 
 const redis = new Redis();
 
-
 const Arweave = require('arweave');
-import moment = require("moment");
 
 dotenv.config({ path: `.env` });
 
 /* ************************** ARWEAVE SETTINGS ************************** */
-// TODO do we need to put this config to .env?
-const ARWEAVE_OPTIONS = {
-  host: 'arweave.net',// Hostname or IP address for a Arweave host
-  port: 443,          // Port
-  protocol: 'https',  // Network protocol http or https
-  timeout: 20000,     // Network request timeouts in milliseconds
-  logging: false,     // Enable network request logging
-}
-const ARWEAVE_ADDRESS = 'QkbQ9Cq7x6I7Jnmw5mePKDSEVavTMvABBPMJY6nGZLY'
 const arweave = Arweave.init(ARWEAVE_OPTIONS);
-const wallet = require(`../${process.env.KEY_PATH}`)
+const wallet = require(`../../${ARWEAVE_OPTIONS.keyPath}`);
 /* ********************************************************************** */
 
 
-export const PENDING_BLOCKS_QUEUE = 'PENDING_BLOCKS_QUEUE'
-export const SAVED_BLOCKS_QUEUE = 'SAVED_BLOCKS_QUEUE'
-
-export const LAST_SAVED_BLOCK_KEY = 'LAST_SAVED_BLOCK_KEY'
+export const PENDING_BLOCKS_QUEUE = 'PENDING_BLOCKS_QUEUE';
+export const SAVED_BLOCKS_QUEUE = 'SAVED_BLOCKS_QUEUE';
+export const LAST_SAVED_BLOCK_KEY = 'LAST_SAVED_BLOCK_KEY';
 
 const pendingBlocksQueue = new Queue(PENDING_BLOCKS_QUEUE);
 const savedBlocksQueue = new Queue(SAVED_BLOCKS_QUEUE);
 
+const compressTxData = (transactions: {
+  transaction: Transaction;
+  meta: ConfirmedTransactionMeta | null;
+}[]) => {
+  // TODO: compress!!! flat buffer? https://github.com/ygoe/msgpack.js/? https://www.npmjs.com/package/msgpack-lite/?
+  return JSON.stringify(transactions);
+};
 
 export async function saveBlockToArweave(solanaBlock: ConfirmedBlock, slotNumber: number) {
-  console.log({ saveSlot: slotNumber })
-
+  console.log(`Save slot ${slotNumber} to arweave`);
+  const { blockhash, parentSlot, transactions } = solanaBlock;
+  const txsData = compressTxData(transactions);
   const arweaveTx = await arweave.createTransaction(
     {
       target: ARWEAVE_ADDRESS,
-      data: `Test string: ${moment.now()}`,
-      // quantity: tokens
+      data: txsData,
     },
     wallet
-  )
-  arweaveTx.addTag('Test-Tag', 'test tag')
+  );
+  // tx tagging
+  arweaveTx.addTag('network', 'testnet'); // TODO: get from options
+  arweaveTx.addTag('blockhash', blockhash);
+  arweaveTx.addTag('parentSlot', parentSlot);
+  arweaveTx.addTag('slot', slotNumber);
 
-  // console.log({ arweaveTx })
-  await arweave.transactions.sign(arweaveTx, wallet)
-  console.log(arweaveTx.id)
+  const txsTags = getTxsTags(transactions);
+  txsTags.forEach((tag) => {
+    arweaveTx.addTag(tag, true);
+  });
+
+  const { data: txPrice } = await getTransactionPrice(arweaveTx.data_size);
+  console.log(`Created arweave tx. Data size: ${arweaveTx.data_size} bytes. Price: ${txPrice} winston (${txPrice / WINSTON_TO_AR} AR)`);
+
+  // await arweave.transactions.sign(arweaveTx, wallet);
+
+  // console.log(arweaveTx.id)
   // const result = await await arweave.transactions.post(arweaveTx)
 
-  await pendingBlocksQueue.add(slotNumber.toString(), {arweaveTx, parentSlot: solanaBlock.parentSlot })
+  // await pendingBlocksQueue.add(slotNumber.toString(), {arweaveTx, parentSlot: solanaBlock.parentSlot })
 }
 
 
@@ -78,13 +87,13 @@ const blockStatusPollingWorker = new Worker(PENDING_BLOCKS_QUEUE, async (job) =>
   } else {
     await pendingBlocksQueue.add(job.name, job.data)
   }
-})
+});
 
 
 const savedBlockWorker = new Worker(SAVED_BLOCKS_QUEUE, async (job) => {
   const lastSavedBlockKey = await redis.get(LAST_SAVED_BLOCK_KEY);
   const savedBlock = job.name
-  const {parentSlot} = job.data
+  const { parentSlot } = job.data
   console.log({ savedBlock, lastSavedBlockKey });
   if (!lastSavedBlockKey) {
     await redis.set(LAST_SAVED_BLOCK_KEY, savedBlock);
@@ -94,4 +103,4 @@ const savedBlockWorker = new Worker(SAVED_BLOCKS_QUEUE, async (job) => {
   } else {
     await savedBlocksQueue.add(job.name, job.data)
   }
-})
+});
