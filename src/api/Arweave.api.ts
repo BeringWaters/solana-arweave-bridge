16d376f04fef889f92f898fb023fffd59cf5c38d
koteld <festbot@gmail.com>
2020-09-28 16:04:01 +0700

    update arweave API

--- src/api/Arweave.api.ts
+++ src/api/Arweave.api.ts
@@ -1,8 +1,53 @@
+const Arweave = require('arweave');
+import { PublicKey } from '@solana/web3.js';
+
 import axios from 'axios';
 import { ARWEAVE_OPTIONS } from '../config';
 
-export const getTransactionPrice = async (txNumberOfBytes: number) => {
-  const response = await axios
-    .get(`${ARWEAVE_OPTIONS.protocol}://${ARWEAVE_OPTIONS.host}/price/${txNumberOfBytes}`);
-  return response;
+const arweave = Arweave.init(ARWEAVE_OPTIONS);
+const wallet = require(`../../${ARWEAVE_OPTIONS.keyPath}`);
+
+export const createTransaction = async (data) => {
+  const arweaveTx = await arweave.createTransaction({ data }, wallet);
+  return arweaveTx;
 };
+
+export const signTransaction = async (tx) => {
+  return arweave.transactions.sign(tx, wallet);
+};
+
+export const postTransaction = async (tx, chunkUploading = true) => {
+  if (chunkUploading) {
+    const uploader = await arweave.transactions.getUploader(tx);
+
+    while (!uploader.isComplete) {
+      await uploader.uploadChunk();
+      console.log(`tx: ${tx.id}: ${uploader.pctComplete}% complete, ${uploader.uploadedChunks}/${uploader.totalChunks}`);
+    }
+  }
+  return arweave.transactions.post(tx);
+};
+
+export const getTransactionPrice = async (byteSize: number) => {
+  const price = await arweave.transactions.getPrice(byteSize);
+  return price;
+};
+
+export const getWalletBalance = async () => {
+  const balance = await arweave.wallets.getBalance(wallet);
+  return balance;
+};
+
+export const getTransactionStatus = async (id) => {
+  const txStatus = await arweave.transactions.getStatus(id);
+  return txStatus;
+};
+
+export default {
+  createTransaction,
+  signTransaction,
+  postTransaction,
+  getTransactionPrice,
+  getWalletBalance,
+  getTransactionStatus
+}
