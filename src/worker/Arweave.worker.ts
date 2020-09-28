48cba7f72a3dd21ad0632781985056e122aa4b93
koteld <festbot@gmail.com>
2020-09-28 16:31:20 +0700

    update solana and arweave workers

--- src/worker/Arweave.worker.ts
+++ src/worker/Arweave.worker.ts
@@ -1,103 +1,186 @@
-import { ConfirmedBlock, ConfirmedTransactionMeta, Transaction } from '@solana/web3.js';
-import * as dotenv from 'dotenv';
-import { Queue, Worker } from 'bullmq';
-import { getTxsTags } from '../service/Arweave.tag.service';
-import { getTransactionPrice } from '../api/Arweave.api';
+import { ConfirmedBlock } from '@solana/web3.js';
+import { Queue, QueueScheduler, Worker, Job } from 'bullmq';
 import * as Redis from 'ioredis';
-import { ARWEAVE_OPTIONS, ARWEAVE_ADDRESS, WINSTON_TO_AR } from '../config';
+import * as msgpack from 'msgpack-lite';
+import {
+  WINSTON_TO_AR,
+  TX_STATUS_POLLING_DELAY,
+  MAX_TAGS_SIZE,
+  TX_STATUS_POLLING_ATTEMPTS,
+  TX_NUMBER_OF_CONFIRMATIONS,
+  MAX_RESPONSE_ATTEMPTS
+} from '../config';
+import { addTagsToTxs } from '../service/Arweave.tag.service';
+import arweaveAPI from '../api/Arweave.api';
 
 const redis = new Redis();
 
-const Arweave = require('arweave');
-import * as msgpack from 'msgpack-lite';
-
-dotenv.config({ path: `.env` });
-
-/* ************************** ARWEAVE SETTINGS ************************** */
-const arweave = Arweave.init(ARWEAVE_OPTIONS);
-const wallet = require(`../../${ARWEAVE_OPTIONS.keyPath}`);
-/* ********************************************************************** */
-
-
-export const PENDING_BLOCKS_QUEUE = 'PENDING_BLOCKS_QUEUE';
-export const SAVED_BLOCKS_QUEUE = 'SAVED_BLOCKS_QUEUE';
+export const POSTING_TXS_QUEUE = 'POSTING_TXS_QUEUE';
+export const PENDING_TXS_QUEUE = 'PENDING_TXS_QUEUE';
+export const SAVED_TXS_QUEUE = 'SAVED_TXS_QUEUE';
 export const LAST_SAVED_BLOCK_KEY = 'LAST_SAVED_BLOCK_KEY';
 
-const pendingBlocksQueue = new Queue(PENDING_BLOCKS_QUEUE);
-const savedBlocksQueue = new Queue(SAVED_BLOCKS_QUEUE);
+const postingTxsQueue = new Queue(POSTING_TXS_QUEUE,{
+  defaultJobOptions: {
+    removeOnComplete: true,
+  },
+});
+const pendingTxsQueue = new Queue(PENDING_TXS_QUEUE);
+const pendingTxsQueueScheduler = new QueueScheduler(PENDING_TXS_QUEUE);
+const savedTxsQueue = new Queue(SAVED_TXS_QUEUE);
 
-const compressTxData = (transactions) => {
+const compressTxsData = (transactions) => {
   return msgpack.encode(transactions);
 };
 
-export async function saveBlockToArweave(solanaBlock: ConfirmedBlock, slotNumber: number) {
-  console.log(`Save slot ${slotNumber} to arweave`);
-  const { blockhash, parentSlot, transactions } = solanaBlock;
-  const txsData = compressTxData(transactions);
-  const arweaveTx = await arweave.createTransaction(
-    {
-      target: ARWEAVE_ADDRESS,
-      data: txsData,
-    },
-    wallet
-  );
-  // tx tagging
-  arweaveTx.addTag('network', 'testnet'); // TODO: get from options
-  arweaveTx.addTag('blockhash', blockhash);
-  arweaveTx.addTag('parentSlot', parentSlot);
-  arweaveTx.addTag('slot', slotNumber);
+const signAndPostTransaction = async (tx) => {
+  await arweaveAPI.signTransaction(tx);
+  // await arweaveAPI.postTransaction(tx);
+};
 
-  const txsTags = getTxsTags(transactions);
-  txsTags.forEach((tag) => {
-    arweaveTx.addTag(tag, true);
+const addTagsToContainer = (containerTags, txTags) => {
+  const tags = containerTags;
+  Object.keys(txTags).forEach((tagKey) => {
+    tags[tagKey] = tags[tagKey] ? [...new Set([...tags[tagKey], ...txTags[tagKey]])] : txTags[tagKey];
+  });
+  return tags;
+};
+
+const getTagsSize = (tags) => {
+  let bytes = 0;
+  Object.keys(tags).forEach((tag) => {
+    const value = tags[tag];
+    if (Array.isArray(value)) {
+      value.forEach((v) => {
+        bytes += v.length + 1;
+      })
+    } else {
+      bytes += value.length + 1;
+    }
+  });
+  return bytes;
+};
+
+const createContainer = (blockhash, slotNumber, containerNumber) => {
+  const tags = {
+    '1': slotNumber,
+    '2': blockhash,
+    '3': containerNumber,
+  };
+
+  return {
+    txs: [],
+    tags,
+    spaceLeft: MAX_TAGS_SIZE - getTagsSize(tags),
+  }
+};
+
+const findBFContainerIndex = (containers, bytes) => {
+  let containerIndex = undefined;
+  containers.forEach((container, index) => {
+    if (container.spaceLeft >= bytes
+      && (!containerIndex || containers[containerIndex].spaceLeft > container.spaceLeft)) {
+      containerIndex = index;
+    }
+  });
+  return containerIndex;
+};
+
+export async function saveBlockToArweave(solanaBlock: ConfirmedBlock, slotNumber: number) {
+  console.log(`Process Solana slot ${slotNumber}`);
+  const { blockhash, transactions: solanaTxs } = solanaBlock;
+  let containerNumber = 0;
+  /**
+   * BF ALLOCATION ALGORITHM
+   */
+  const taggedTxs = addTagsToTxs(solanaTxs);
+  const txContainers = taggedTxs.reduce((txContainers, taggedTx) => {
+    const { tags, transaction, bytes} = taggedTx;
+    let txContainerIndex = findBFContainerIndex(txContainers, bytes);
+    if (!txContainerIndex) {
+      txContainerIndex = (txContainers.push(createContainer(blockhash, slotNumber, containerNumber++)) - 1);
+    }
+    txContainers[txContainerIndex].txs.push(transaction);
+    txContainers[txContainerIndex].tags = addTagsToContainer(txContainers[txContainerIndex].tags, tags);
+    txContainers[txContainerIndex].spaceLeft -= bytes;
+  }, [createContainer(blockhash, slotNumber, containerNumber++)]);
+
+  await Promise.all(txContainers.forEach(async (container) => {
+    container.txs = await compressTxsData(container.txs);
+    await postingTxsQueue.add(`${container.tags['1']}_${container.tags['2']}`, container);
+  }))
+}
+
+const txPostingWorker = new Worker(POSTING_TXS_QUEUE, async (job: Job) => {
+  const { data: container } = job;
+  const { txs, tags } = container;
+  const arweaveTx = await arweaveAPI.createTransaction({data: txs});
+  Object.keys(tags).forEach((tagKey) => {
+    const values = tags[tagKey];
+    values.forEach((value => arweaveTx.addTag(tagKey, value)));
   });
 
-  const { data: txPrice } = await getTransactionPrice(arweaveTx.data_size);
-  console.log(`Created arweave tx. Data size: ${arweaveTx.data_size} bytes. Price: ${txPrice} winston (${txPrice / WINSTON_TO_AR} AR)`);
+  const txPrice = await arweaveAPI.getTransactionPrice(arweaveTx.data_size);
+  const balance = await arweaveAPI.getWalletBalance();
 
-  // await arweave.transactions.sign(arweaveTx, wallet);
+  if (balance < txPrice) {
+    console.log(`Wallet balance is not sufficient to process arweave transaction ${arweaveTx.id}.\n
+    Data size: ${arweaveTx.data_size} bytes. Price: ${txPrice} winston (${txPrice / WINSTON_TO_AR} AR)`);
+    await postingTxsQueue.add(`${container.tags['1']}_${container.tags['2']}`, container);
+    return;
+  }
 
-  // console.log(arweaveTx.id)
-  // const result = await await arweave.transactions.post(arweaveTx)
+  await signAndPostTransaction(arweaveTx);
+  await pendingTxsQueue.add(`${container.tags['1']}_${container.tags['2']}`, {arweaveTx, container}, {
+    delay: TX_STATUS_POLLING_DELAY,
+    attempts: MAX_RESPONSE_ATTEMPTS,
+  });
+  console.log(`Created arweave tx: ${arweaveTx.id}. Data size: ${arweaveTx.data_size} bytes. Price: ${txPrice} winston (${txPrice / WINSTON_TO_AR} AR)`);
+});
 
-  // await pendingBlocksQueue.add(slotNumber.toString(), {arweaveTx, parentSlot: solanaBlock.parentSlot })
-}
+const txStatusPollingWorker = new Worker(PENDING_TXS_QUEUE, async (job: Job) => {
+  const { data } = job;
+  const { arweaveTx } = data;
+  console.log(`Check arweave transaction status: ${arweaveTx.id}`);
+  const { confirmed } = await arweaveAPI.getTransactionStatus(arweaveTx.id);
 
+  if (!confirmed) {
+    throw new Error(`Error occurred while posting transaction ${arweaveTx.id} to Arweave`);
+  }
 
-async function checkArweaveTxStatus(job) {
-  const txData = job.data;
-  const { id } = txData;
-  // test
-  return Math.random() > 0.8;
-}
-
-const blockStatusPollingWorker = new Worker(PENDING_BLOCKS_QUEUE, async (job) => {
-  console.log({ checkStatus: job.name });
-  const hasSuccessStatus = await checkArweaveTxStatus(job)
-  if (hasSuccessStatus) {
-    await savedBlocksQueue.add(job.name, job.data)
-  } else {
-    await pendingBlocksQueue.add(job.name, job.data)
+  if (confirmed.number_of_confirmations < TX_NUMBER_OF_CONFIRMATIONS) {
+    throw new Error(`Insufficient number of confirmations for transaction ${arweaveTx.id}`);
   }
 });
 
+txStatusPollingWorker.on('completed', async (job: Job) => {
+  const { data } = job;
+  const { arweaveTx, container } = data;
+  await savedTxsQueue.add(`${container.tags['1']}_${container.tags['2']}`, arweaveTx);
+});
 
-const savedBlockWorker = new Worker(SAVED_BLOCKS_QUEUE, async (job) => { // FIXME THIS WORKER WORKS NOT CORRECT. NEED TO DEBUG...
-  const lastSavedBlock = await redis.get(LAST_SAVED_BLOCK_KEY);
-  const savedBlock = job.name;
-  const { parentSlot } = job.data;
-  console.log({ savedBlock, lastSavedBlock, parentSlot });
-  let jobs = await savedBlocksQueue.getJobs(['waiting', 'active' ]);
-  console.log({jobs: jobs.map(job => job.name)});
-  if(Number.parseInt(lastSavedBlock) >  Number.parseInt(savedBlock)) {
-    return
-  }
-  if (!lastSavedBlock) {
-    await redis.set(LAST_SAVED_BLOCK_KEY, savedBlock);
-  } else if (Number.parseInt(lastSavedBlock) === parentSlot) {
-    await redis.set(LAST_SAVED_BLOCK_KEY, savedBlock);
-    console.log({ LAST_SAVED_BLOCK_KEY: savedBlock });
-  } else {
-    await savedBlocksQueue.add(job.name, job.data, {lifo: true});
-  }
+txStatusPollingWorker.on('failed', async (job: Job) => {
+  const { data } = job;
+  const { arweaveTx, container } = data;
+  await postingTxsQueue.add(`${container.tags['1']}_${container.tags['2']}`, arweaveTx);
+});
+
+const savedBlockWorker = new Worker(SAVED_TXS_QUEUE, async (job) => { // FIXME THIS WORKER WORKS NOT CORRECT. NEED TO DEBUG...
+  // const lastSavedBlock = await redis.get(LAST_SAVED_BLOCK_KEY);
+  // const savedBlock = job.name;
+  // const { parentSlot } = job.data;
+  // console.log({ savedBlock, lastSavedBlock, parentSlot });
+  // let jobs = await savedBlocksQueue.getJobs(['waiting', 'active' ]);
+  // console.log({jobs: jobs.map(job => job.name)});
+  // if(Number.parseInt(lastSavedBlock) >  Number.parseInt(savedBlock)) {
+  //   return
+  // }
+  // if (!lastSavedBlock) {
+  //   await redis.set(LAST_SAVED_BLOCK_KEY, savedBlock);
+  // } else if (Number.parseInt(lastSavedBlock) === parentSlot) {
+  //   await redis.set(LAST_SAVED_BLOCK_KEY, savedBlock);
+  //   console.log({ LAST_SAVED_BLOCK_KEY: savedBlock });
+  // } else {
+  //   await savedBlocksQueue.add(job.name, job.data, {lifo: true});
+  // }
 });
--- src/worker/Solana.worker.ts
+++ src/worker/Solana.worker.ts
@@ -1,58 +1,85 @@
 import {
   LAST_SAVED_BLOCK_KEY,
-  PENDING_BLOCKS_QUEUE,
+  PENDING_TXS_QUEUE,
   saveBlockToArweave,
-  SAVED_BLOCKS_QUEUE
+  SAVED_TXS_QUEUE
 } from './Arweave.worker';
-import { Queue } from 'bullmq';
-import { getConfirmedSlots, getConfirmedBlock, getCurrentSlot, getFirstSlot } from '../api/Solana.api';
-import { MAX_SLOT_CHUNK_SIZE } from '../config';
+import {Job, Queue, Worker} from 'bullmq';
+import { getConfirmedBlocks, getConfirmedBlock, getCurrentSlot, getFirstSlot } from '../api/Solana.api';
+import {MAX_SLOT_CHUNK_SIZE, TX_NUMBER_OF_CONFIRMATIONS} from '../config';
 
 import * as Redis from 'ioredis';
+import arweaveAPI from "../api/Arweave.api";
 const redis = new Redis();
+
+export const FETCHING_TXS_QUEUE = 'FETCHING_TXS_QUEUE';
+const fetchingTxsQueue = new Queue(FETCHING_TXS_QUEUE);
 
 const redisCleanupOnRestart = async () => {
   await redis.set(LAST_SAVED_BLOCK_KEY, null); // FIXME need this value on restart
-  const pendingBlocksQueue = new Queue(PENDING_BLOCKS_QUEUE);
-  const savedBlocksQueue = new Queue(SAVED_BLOCKS_QUEUE);
+  const pendingBlocksQueue = new Queue(PENDING_TXS_QUEUE);
+  const savedBlocksQueue = new Queue(SAVED_TXS_QUEUE);
   await pendingBlocksQueue.clean(1000, 1000);
   await savedBlocksQueue.clean(1000, 1000);
 };
 
-export const start = async () => {
+export const start = async (options = {
+  firstSlot: 36568072,
+  lastSlot: undefined,
+  concurrency: 8,
+}) => {
+  const start = new Date();
+  console.log(start);
+
   console.log('Arweave initialization...');
   console.log('Blocks fetching started...');
 
   await redisCleanupOnRestart();
 
-  const { result: firstSlot } = await getFirstSlot(); // get from options
-  const { result: lastSlot } = { result: firstSlot + 200 } || await getCurrentSlot(); // get from options
+  const firstSlot = options.firstSlot || await getFirstSlot();
+  const lastSlot = options.lastSlot || await getCurrentSlot();
 
+  /**
+  * Get all confirmed Solana slots
+  */
   const confirmedSlots = [];
   let lastFetchedSlot = firstSlot;
+
+  console.log(`Fetching confirmed blocks:\n
+  ${0}/${lastSlot - firstSlot}\n`);
 
   try {
     while (lastFetchedSlot - lastSlot <= MAX_SLOT_CHUNK_SIZE) {
       const lastSlotToFetch = lastFetchedSlot + MAX_SLOT_CHUNK_SIZE;
-      const { result: confirmedSlotsChunk } = await getConfirmedSlots(lastFetchedSlot, lastSlotToFetch > lastSlot ? lastSlot : lastSlotToFetch);
+      const { result: confirmedSlotsChunk } = await getConfirmedBlocks(lastFetchedSlot, lastSlotToFetch > lastSlot ? lastSlot : lastSlotToFetch);
       confirmedSlots.push(...confirmedSlotsChunk);
       lastFetchedSlot = lastSlotToFetch;
+      console.log(`${lastFetchedSlot - firstSlot}/${lastSlot - firstSlot}\n`)
     }
   } catch (e) {
     console.log(`Failed to get confirmed slots: ${e.message}`);
     return;
   }
 
-  // add job that enlarges confirmedBlocks with getCurrentSlot() + getConfirmedBlocks() if lastBlock not in options
+  // TODO: add job that enlarges confirmedBlocks with getCurrentSlot() + getConfirmedBlocks() if lastBlock not in options
 
-  for (const slot of confirmedSlots) {
-    try {
-      const { result: confirmedBlock } = await getConfirmedBlock(slot);
-      await saveBlockToArweave(confirmedBlock, slot);
-      console.log(`Last saved slot: ${slot}`); // TODO: save last saved slot
-    } catch (e) {
-      console.log(`Failed to proceed slot ${slot}: ${e.message}`);
-    }
-    console.log('\n');
-  }
+  confirmedSlots.forEach(async (slot) => {
+    await fetchingTxsQueue.add(`${slot}`, slot)
+  })
 };
+
+const txFetchingWorker = new Worker(FETCHING_TXS_QUEUE, async (job: Job) => {
+  const { data: slot } = job;
+  try {
+    const { result: confirmedBlock } = await getConfirmedBlock(slot);
+    await saveBlockToArweave(confirmedBlock, slot);
+    console.log(`Last fetched slot: ${slot}`);
+  } catch (e) {
+    throw Error(`Failed to proceed slot ${slot}: ${e.message}`);
+  }
+});
+
+txFetchingWorker.on('failed', async (job: Job) => {
+  const { data: slot } = job;
+  await fetchingTxsQueue.add(`${slot}`, slot);
+});
