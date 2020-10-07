59bc93faf783417180ba7a9fac34595aafc1e18c
Vladimir Popovich <vladimir.popovich@exyte.com>
2020-10-07 17:23:00 +0700

    Fix lint errors

--- src/config.ts
+++ src/config.ts
@@ -1,44 +1,45 @@
-import * as dotenv from 'dotenv'
-dotenv.config({ path: `.env` });
+import * as dotenv from 'dotenv';
 
-export let OPTIONS = {
-    firstSlot: undefined,
-    lastSlot: undefined,
-    livestream: false,
-    database: process.env.DATABASE || 't',
-    concurrency: parseInt(process.env.CONCURRENCY, 10) || 4,
-    local: false,
-    compressed: false,
-    verify: true,
-    benchmark: false,
-    keyPath: process.env.ARWEAVE_KEY_PATH,
+dotenv.config({ path: '.env' });
+
+export const OPTIONS = {
+  firstSlot: undefined,
+  lastSlot: undefined,
+  livestream: false,
+  database: process.env.DATABASE || 't',
+  concurrency: parseInt(process.env.CONCURRENCY, 10) || 4,
+  local: false,
+  compressed: false,
+  verify: true,
+  benchmark: false,
+  keyPath: process.env.ARWEAVE_KEY_PATH,
 };
 
 export const updateOptions = (params = {}) => {
-    Object.keys(params).forEach((key) => {
-        if (params[key] === undefined) return;
-        OPTIONS[key] = params[key];
-    })
+  Object.keys(params).forEach((key) => {
+    if (params[key] === undefined) return;
+    OPTIONS[key] = params[key];
+  });
 };
 
 export const ARWEAVE_OPTIONS = {
-    host: 'arweave.net',
-    port: 443,
-    protocol: 'https',
-    timeout: 20000,
-    logging: false,
+  host: 'arweave.net',
+  port: 443,
+  protocol: 'https',
+  timeout: 20000,
+  logging: false,
 };
 
-export let SOLANA_OPTIONS = {
-    url: process.env.SOLANA_NODE_URL || 'https://testnet.solana.com',
-    jsonrpc: process.env.SOLANA_RPC || '2.0',
+export const SOLANA_OPTIONS = {
+  url: process.env.SOLANA_NODE_URL || 'https://testnet.solana.com',
+  jsonrpc: process.env.SOLANA_RPC || '2.0',
 };
 
 export const updateSolanaOptions = (params = {}) => {
-    Object.keys(params).forEach((key) => {
-        if (params[key] === undefined) return;
-        SOLANA_OPTIONS[key] = params[key];
-    })
+  Object.keys(params).forEach((key) => {
+    if (params[key] === undefined) return;
+    SOLANA_OPTIONS[key] = params[key];
+  });
 };
 
 export const MAX_SLOT_CHUNK_SIZE = parseInt(process.env.MAX_SLOT_CHUNK_SIZE, 10) || 100000;
@@ -47,7 +48,8 @@ export const MAX_RESPONSE_ATTEMPTS = parseInt(process.env.MAX_RESPONSE_ATTEMPTS,
 
 export const TX_STATUS_POLLING_DELAY = parseInt(process.env.TX_STATUS_POLLING_DELAY, 10) || 1000;
 
-export const TX_STATUS_POLLING_ATTEMPTS = parseInt(process.env.TX_STATUS_POLLING_ATTEMPTS, 10) || 960;
+export const TX_STATUS_POLLING_ATTEMPTS = parseInt(process.env.TX_STATUS_POLLING_ATTEMPTS, 10)
+    || 960;
 
 export const TX_NUMBER_OF_CONFIRMATIONS = parseInt(process.env.TX_NUMBER_OF_CONFIRMATIONS, 10) || 1;
 
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
+/* eslint-disable no-await-in-loop,import/no-named-as-default-member,no-console */
 import { Job } from 'bullmq';
 import arweaveAPI from './api/Arweave.api';
 import solanaAPI from './api/Solana.api';
 import { MAX_SLOT_CHUNK_SIZE, OPTIONS } from './config';
 import { redis, redisCleanup } from './service/Redis.service';
-import { LAST_SAVED_SLOT } from './service/Queue.service';
-import { fetchingTxsQueue, WALLET_BALANCE, walletBalanceQueue } from './service/Queue.service';
+import {
+  LAST_SAVED_SLOT, fetchingTxsQueue, WALLET_BALANCE, walletBalanceQueue,
+} from './service/Queue.service';
+
 import { txPostingWorker } from './worker/Arweave.posting.worker';
 import { txFetchingWorker } from './worker/Solana.fetching.worker';
 import { txStatusPollingWorker } from './worker/Arweave.polling.worker';
@@ -16,10 +19,12 @@ const fetchSlots = async (first, last) => {
   try {
     while (lastFetchedSlot - last < MAX_SLOT_CHUNK_SIZE) {
       const lastSlotToFetch = lastFetchedSlot + MAX_SLOT_CHUNK_SIZE;
-      const { result: confirmedSlotsChunk } = await solanaAPI.getConfirmedBlocks(lastFetchedSlot, lastSlotToFetch > last ? last : lastSlotToFetch);
+      const { result: confirmedSlotsChunk } = await solanaAPI.getConfirmedBlocks(
+        lastFetchedSlot, lastSlotToFetch > last ? last : lastSlotToFetch,
+      );
       fetchedSlots.push(...confirmedSlotsChunk);
       lastFetchedSlot = lastSlotToFetch;
-      console.log(`${fetchedSlots.length}/${last - first + 1}`)
+      console.log(`${fetchedSlots.length}/${last - first + 1}`);
     }
   } catch (e) {
     console.log(`Failed to get confirmed slots: ${e.message}`);
@@ -28,23 +33,24 @@ const fetchSlots = async (first, last) => {
   return fetchedSlots;
 };
 
+// eslint-disable-next-line import/prefer-default-export
 export const start = async () => {
   await redisCleanup();
   const balance = await arweaveAPI.getWalletBalance();
   await redis.set(WALLET_BALANCE, balance);
   console.log(`Wallet balance ${balance} winston`);
-  await walletBalanceQueue.add(WALLET_BALANCE, {},{
+  await walletBalanceQueue.add(WALLET_BALANCE, {}, {
     repeat: {
       every: 60000,
-    }
+    },
   });
 
   console.log('Arweave initialization...');
   console.log('Blocks fetching started...');
 
   const livestream = OPTIONS.livestream && !(OPTIONS.lastSlot === undefined);
-  const firstSlot = parseInt(OPTIONS.firstSlot,10) || await solanaAPI.getCurrentSlot();
-  const lastSlot = parseInt(OPTIONS.lastSlot,  10) || firstSlot;
+  const firstSlot = parseInt(OPTIONS.firstSlot, 10) || await solanaAPI.getCurrentSlot();
+  const lastSlot = parseInt(OPTIONS.lastSlot, 10) || firstSlot;
 
   /**
    * Get all confirmed Solana slots
@@ -83,9 +89,10 @@ export const start = async () => {
   txPostingWorker.opts.concurrency = concurrency;
   txStatusPollingWorker.opts.concurrency = concurrency;
 
-  const length = confirmedSlots.length;
+  const { length } = confirmedSlots;
+  // eslint-disable-next-line no-plusplus
   for (let i = 0; i < (concurrency * 10) && i < length; ++i) {
     const slot = confirmedSlots.shift();
-    await fetchingTxsQueue.add(`${slot}`, slot)
+    await fetchingTxsQueue.add(`${slot}`, slot);
   }
 };
