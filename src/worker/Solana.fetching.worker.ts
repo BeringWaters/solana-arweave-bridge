03b76963f374482e19ddd5f7ae33cd7a851219bf
koteld <festbot@gmail.com>
2020-10-20 01:56:06 +0700

    update and refactor workers

--- src/worker/Solana.fetching.worker.ts
+++ src/worker/Solana.fetching.worker.ts
@@ -1,42 +1,65 @@
 import { ConfirmedBlock } from '@solana/web3.js';
-import {Job, Worker} from 'bullmq';
+import { Job, Worker } from 'bullmq';
 import solanaAPI from '../api/Solana.api';
 import arweaveAPI from '../api/Arweave.api';
+
 import { createContainers } from '../service/Arweave.BFA.service';
 import { compressData } from '../service/Arweave.compression.service';
 import {
   postingTxsQueue,
+  fetchingTxsQueue,
   FETCHING_TXS_QUEUE,
 } from '../service/Queue.service';
+import { redis, SAVED_BLOCKS_SET } from '../service/Redis.service';
 import { BLOCK_TAGS } from '../constants';
 import { OPTIONS } from '../config';
 
-async function postBlockToArweave(solanaBlock: ConfirmedBlock, slotNumber: number) {
-  console.log(`Process Solana slot ${slotNumber}`);
+async function postBlockToArweave(solanaBlock: ConfirmedBlock, blockNumber: number) {
+  console.log(`Processing block ${blockNumber}`);
   try {
-    const txContainers = await createContainers(solanaBlock, slotNumber);
-    await Promise.all(txContainers.map(async (container) => {
+    const txContainers = await createContainers(solanaBlock, blockNumber);
+    return await Promise.all(txContainers.map(async (container) => {
       if (OPTIONS.verify) {
         const result = await arweaveAPI.searchContainer(container.tags);
         if (result.length > 0) return;
       }
       container.txs = await compressData(container.txs);
 
-      return (await postingTxsQueue.add(`${container.tags[BLOCK_TAGS['slot'].alias]}_${container.tags[BLOCK_TAGS['container'].alias]}`, container));
+      const containerNumber = container.tags[BLOCK_TAGS['container'].alias];
+      const database = container.tags[BLOCK_TAGS['database'].alias];
+      const network = container.tags[BLOCK_TAGS['network'].alias];
+
+      const txIdentifier = `${blockNumber}_${containerNumber}_${database}_${network}`;
+      const processed = await redis.sismember(SAVED_BLOCKS_SET, txIdentifier);
+      if (processed) {
+        console.log(`Block ${blockNumber} (part ${containerNumber}) is already stored in Arweave`);
+        return;
+      }
+
+      return (await postingTxsQueue.add(txIdentifier, container));
     }));
   } catch (e) {
-    console.log(`Error occurred while processing slot ${slotNumber}: ${e}`);
+    console.log(`Error occurred while processing block ${blockNumber}: ${e}`);
     return;
   }
 }
 
 export const txFetchingWorker = new Worker(FETCHING_TXS_QUEUE, async (job: Job) => {
-  const { data: slot } = job;
+  const { data: blockNumber } = job;
   try {
-    const { result: confirmedBlock } = await solanaAPI.getConfirmedBlock(slot);
-    await postBlockToArweave(confirmedBlock, slot);
-    console.log(`Last proceeded slot: ${slot}`);
+    const { result: confirmedBlock } = await solanaAPI.getConfirmedBlock(blockNumber);
+    await postBlockToArweave(confirmedBlock, blockNumber);
   } catch (e) {
-    throw Error(`Failed to proceed slot ${slot}: ${e.message}`);
+    throw Error(`Failed to process block ${blockNumber}: ${e.message}`);
   }
+});
+
+txFetchingWorker.on('completed', async (job: Job) => {
+  const { data: blockNumber } = job;
+  console.log(`Fetched block ${blockNumber} successfully`);
+});
+
+txFetchingWorker.on('failed', async (job: Job) => {
+  const { data: blockNumber } = job;
+  await fetchingTxsQueue.add(`${blockNumber}`, blockNumber);
 });
--- src/worker/WalletBalance.worker.ts
+++ src/worker/WalletBalance.worker.ts
@@ -1,9 +1,10 @@
 import { Worker } from 'bullmq';
-import { redis } from '../service/Redis.service';
-import { WALLET_BALANCE } from '../service/Queue.service';
 import arweaveAPI from '../api/Arweave.api';
 
-export const walletBalanceWorker = new Worker(WALLET_BALANCE, async () => {
+import { redis, WALLET_BALANCE } from '../service/Redis.service';
+import { FETCHING_BALANCE_QUEUE } from '../service/Queue.service';
+
+export const walletBalanceWorker = new Worker(FETCHING_BALANCE_QUEUE, async () => {
   const balance = await arweaveAPI.getWalletBalance();
   await redis.set(WALLET_BALANCE, balance);
 });
