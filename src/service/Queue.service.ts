fa74da2abaa7eaf28ba05fdc0f89b5cfd1d62bf9
koteld <festbot@gmail.com>
2020-10-20 01:55:44 +0700

    add services initialization; add redis and queue services cleanup functions; split redis and queue services consts

--- src/service/Queue.service.ts
+++ src/service/Queue.service.ts
@@ -1,18 +1,32 @@
 import { Queue, QueueScheduler } from 'bullmq';
+import { REDIS_OPTIONS } from '../config';
 
 export const FETCHING_TXS_QUEUE = 'FETCHING_TXS_QUEUE';
+export const FETCHING_BALANCE_QUEUE = 'FETCHING_BALANCE_QUEUE';
 export const POSTING_TXS_QUEUE = 'POSTING_TXS_QUEUE';
 export const PENDING_TXS_QUEUE = 'PENDING_TXS_QUEUE';
-export const SAVED_TXS_QUEUE = 'SAVED_TXS_QUEUE';
-export const WALLET_BALANCE = 'WALLET_BALANCE';
 
-export const AR_SPENT = 'AR_SPENT';
-export const AR_SPENT_UNCONFIRMED = 'AR_SPENT_UNCONFIRMED';
-export const LAST_SAVED_SLOT = 'LAST_SAVED_SLOT';
+export let fetchingTxsQueue;
+export let postingTxsQueue;
+export let pendingTxsQueue;
+export let pendingTxsQueueScheduler;
+export let fetchingBalanceQueue;
 
-export const fetchingTxsQueue = new Queue(FETCHING_TXS_QUEUE, { defaultJobOptions: { timeout: 60000 } });
-export const postingTxsQueue = new Queue(POSTING_TXS_QUEUE,{defaultJobOptions: { removeOnComplete: true } });
-export const pendingTxsQueue = new Queue(PENDING_TXS_QUEUE);
-export const pendingTxsQueueScheduler = new QueueScheduler(PENDING_TXS_QUEUE);
-export const savedTxsQueue = new Queue(SAVED_TXS_QUEUE);
-export const walletBalanceQueue = new Queue(WALLET_BALANCE, { defaultJobOptions: { timeout: 60000 } });
+export const initializeQueueService = async () => {
+	const connection = REDIS_OPTIONS;
+	fetchingTxsQueue = new Queue(FETCHING_TXS_QUEUE, { connection, defaultJobOptions: { timeout: 60000 } } );
+	fetchingBalanceQueue = new Queue(FETCHING_BALANCE_QUEUE, { connection, defaultJobOptions: { timeout: 60000 } } );
+	postingTxsQueue = new Queue(POSTING_TXS_QUEUE,{ connection, defaultJobOptions: { removeOnComplete: true } } );
+	pendingTxsQueue = new Queue(PENDING_TXS_QUEUE, { connection });
+	pendingTxsQueueScheduler = new QueueScheduler(PENDING_TXS_QUEUE, { connection });
+};
+
+export const queueCleanup = async () => {
+	await fetchingTxsQueue.clean(0,0,  'wait');
+	await fetchingTxsQueue.clean(0,0,  'active');
+	await fetchingTxsQueue.clean(0,0,  'delayed');
+
+	await postingTxsQueue.clean(0,0,  'wait');
+	await postingTxsQueue.clean(0,0,  'active');
+	await postingTxsQueue.clean(0,0,  'delayed');
+};
